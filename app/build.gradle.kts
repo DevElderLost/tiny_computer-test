1081f732b16fd320989f955bc22fb704ae0732e6
DevElderLost <128934990+DevElderLost@users.noreply.github.com>
2026-07-05 06:04:50 +0000

    delete: blurview terminal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,7 +79,6 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.material)
     implementation(libs.viewpagerindicator)
-    implementation(libs.blurview)
     implementation(libs.snakeyaml)
     implementation(libs.coil)
     implementation(libs.coil.gif)
